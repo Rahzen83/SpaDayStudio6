72ca130cb1a5d6d6bacce3d01092fc3af049b08d
Richard <rahzen83@gmail.com>
2023-03-28 15:54:43 -0500

    Finished studio

---
+++ SpaDay6/Controllers/UserController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SpaDay6.Models;
+
+namespace SpaDay6.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("/user/add")]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("/user")]
+        public IActionResult SubmitAddUserForm(User newUser, string verify)
+        {
+            if (verify == newUser.Password)
+            {
+                ViewBag.username = newUser.Username;
+                return View("Index");
+            }
+            else
+            {
+                ViewBag.username = newUser.Username;
+                ViewBag.email = newUser.Email;
+                ViewBag.error = "The Passwords Don't Match, Airhead.";
+                return View("Add");
+            }
+        }
+
+    }
+}
---
+++ SpaDay6/Models/User.cs
@@ -0,0 +1,9 @@
+﻿namespace SpaDay6.Models
+{
+    public class User
+    {
+        public string Username { get; set; }  
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
